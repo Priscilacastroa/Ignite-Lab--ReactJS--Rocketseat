721b16bd819e9367aa2ca9c031e432ecb1098564
Priscila Castro <priscilacastroa@gmail.com>
2022-06-29 19:52:49 -0300

    Subscribe

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: "https://api-sa-east-1.graphcms.com/v2/cl4q8d9kl38qg01xk0wajgmzl/master",
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        "Authorization": `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN} ` 
+    },
     cache: new InMemoryCache()
-})
+}) 
